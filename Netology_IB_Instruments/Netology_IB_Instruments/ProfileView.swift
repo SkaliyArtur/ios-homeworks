a6720be6a0591423ff81186dd6a9a3f91126d3d1
Artur Skaliy <skaliy.art@gmail.com>
2021-08-17 19:25:24 +1000

    Выполнил все пункты из ДЗ 1.2

---
+++ Netology_IB_Instruments/Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Artur Skaliy on 17.08.2021.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+    @IBOutlet weak var nameUILabel: UILabel!
+    @IBOutlet weak var birthdayUILabel: UILabel!
+    @IBOutlet weak var cityUILable: UILabel!
+    @IBOutlet weak var avatarUIImageView: UIImageView!
+    @IBOutlet weak var signatureUITextView: UITextView!
+    
+}
