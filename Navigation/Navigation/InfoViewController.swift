cbd92dcc790f58c3d292eb4ff6f05d461ae676d5
SkaliyArtur <skaliy.art@gmail.com>
2022-10-08 23:44:47 +0300

    ДЗ 1.2 задача 2

--- Navigation/Navigation/InfoViewController.swift
+++ Navigation/Navigation/InfoViewController.swift
@@ -9,8 +9,8 @@ import UIKit
 
 class InfoViewController: UIViewController {
     
-    let jsonLabel = UILabel()
-    let group = DispatchGroup()
+    let jsonLabel1 = UILabel()
+    let jsonLabel2 = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,12 +20,19 @@ class InfoViewController: UIViewController {
         alertButton.addTarget(self, action: #selector(tap), for: .touchUpInside)
         view.addSubview(alertButton)
         
-        guard let url = URL(string: "https://jsonplaceholder.typicode.com/todos/\(Int.random(in: 1...200))") else {return}
-        dataTaskJSONSerialization(url)
-        jsonLabel.frame = CGRect(x: 16, y: 500, width: 400, height: 30)
-        jsonLabel.textColor = .white
-        view.addSubview(jsonLabel)
+        guard let url1 = URL(string: "https://jsonplaceholder.typicode.com/todos/\(Int.random(in: 1...200))") else {return}
+        dataTaskJSONSerialization(url1)
+        jsonLabel1.frame = CGRect(x: 16, y: 500, width: 400, height: 30)
+        jsonLabel1.textColor = .white
+        view.addSubview(jsonLabel1)
+        
+        guard let url2 = URL(string: "https://swapi.dev/api/planets/1") else {return}
+        dataTaskJSONDecoder(url2)
+        jsonLabel2.frame = CGRect(x: 16, y: 600, width: 400, height: 30)
+        jsonLabel2.textColor = .white
+        view.addSubview(jsonLabel2)
     }
+    
     @objc func tap() {
         let alertVC = UIAlertController(title: "Это Alert", message: "Так попросили сделать в домашке", preferredStyle: .actionSheet)
         let actionOne = UIAlertAction(title: "OK", style: .default, handler: {(UIAlertAction) in print("Is's OK")})
@@ -33,7 +40,6 @@ class InfoViewController: UIViewController {
         alertVC.addAction(actionOne)
         alertVC.addAction(actionTwo)
         self.present(alertVC, animated: true, completion: nil)
-        
     }
 
     func dataTaskJSONSerialization(_ address: URL) {
@@ -45,9 +51,25 @@ class InfoViewController: UIViewController {
                     guard let title = json["title"] as? String else {return}
                     print("title \(title)")
                     DispatchQueue.main.async {
-                        self.jsonLabel.text = title
-                        print("JSONLABEL \(String(describing: self.jsonLabel.text))")
+                        self.jsonLabel1.text = title
+                        print("JSONLABEL \(String(describing: self.jsonLabel1.text))")
                     }
+                }
+            } catch let error as NSError {
+                    print("error: \(error.localizedDescription)")
+            }
+        }
+        task.resume()
+    }
+    
+    func dataTaskJSONDecoder(_ address: URL) {
+        let session = URLSession.shared
+        let task = session.dataTask(with: address) {data, _, error in
+            do {
+                guard let data = data else { return }
+                let model = try JSONDecoder().decode(JSONModel.self, from: data)
+                DispatchQueue.main.async {
+                    self.jsonLabel2.text = "Период обращение планеты \(model.name) = \(model.orbitalPeriod)"
                 }
             } catch let error as NSError {
                     print("error: \(error.localizedDescription)")
