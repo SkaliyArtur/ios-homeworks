0d4ff300ea5c17e42a7f8df105e3107338a66dfd
SkaliyArtur <skaliy.art@gmail.com>
2023-01-29 23:53:01 +0300

    ДЗ 4: сделал задачи 1-3

---
+++ SwiftUI_netology/SwiftUI_netology/SwiftUI_netologyApp.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUI_netologyApp.swift
+//  SwiftUI_netology
+//
+//  Created by Artur Skaliy on 29.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_netologyApp: App {
+    var body: some Scene {
+        WindowGroup {
+//            ContentView()
+            TabBarView()
+        }
+    }
+}
